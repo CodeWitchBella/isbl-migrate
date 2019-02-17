6382886cb26b352c210b9859fa85b3fff08c3dea
Isabella Skořepová <isabella@skorepova.info>
2019-02-17 18:32:29 +0100

    Only run down-up logic on last migration

--- src/migrate.ts
+++ src/migrate.ts
@@ -57,47 +57,53 @@ async function handleAlreadyRun({
   migration,
   development,
   knex,
+  last,
 }: {
   data: DbMigration[]
   migration: any
   development: boolean
   knex: Knex
+  last: boolean
 }) {
   const prevIdx = data.findIndex(mig => mig.name === migration.name)
   if (prevIdx >= 0) {
     let prev = data[prevIdx]
-    if (prev.up !== migration.up.toString()) {
-      if (development) {
-        await knex.transaction(async trx => {
-          console.log(`Migration ${migration.name} up changed`)
-          console.log('Running down migration and following it with up again')
-          console.log('Running down from database')
-          await trx.raw(prev.down).catch(() => {
-            console.log('Down from database failed. Running down from disc')
-            return trx.raw(migration.down)
+    // only test the last migration because codegen could easily change and if
+    // it does then we dont want to trigger massive failing down/up cycles
+    if (last) {
+      if (prev.up !== migration.up.toString()) {
+        if (development) {
+          await knex.transaction(async trx => {
+            console.log(`Migration ${migration.name} up changed`)
+            console.log('Running down migration and following it with up again')
+            console.log('Running down from database')
+            await trx.raw(prev.down).catch(() => {
+              console.log('Down from database failed. Running down from disc')
+              return trx.raw(migration.down)
+            })
+
+            console.log('Running up')
+            await trx.raw(migration.up)
+
+            prev = migrationToDB(migration)
+            // eslint-disable-next-line no-param-reassign
+            data[prevIdx] = prev
+            await save(data, knex)
           })
-
-          console.log('Running up')
-          await trx.raw(migration.up)
-
-          prev = migrationToDB(migration)
-          // eslint-disable-next-line no-param-reassign
-          data[prevIdx] = prev
-          await save(data, knex)
-        })
-      } else {
-        throw new Error(
-          'Up migration changed! This is bad in non-development env',
-        )
+        } else {
+          throw new Error(
+            'Up migration changed! This is bad in non-development env',
+          )
+        }
       }
-    }
-    if (
-      prev.down !== migration.down ||
-      prev.description !== migration.description
-    ) {
-      // eslint-disable-next-line no-param-reassign
-      data[prevIdx] = migrationToDB(migration)
-      await save(data, knex)
+      if (
+        prev.down !== migration.down ||
+        prev.description !== migration.description
+      ) {
+        // eslint-disable-next-line no-param-reassign
+        data[prevIdx] = migrationToDB(migration)
+        await save(data, knex)
+      }
     }
     return true
   }
@@ -147,8 +153,18 @@ export async function migrate({
 
   // run migrations which were not run yet
   migrations.reverse()
+  let i = 0
   for (const migration of migrations) {
-    if (await handleAlreadyRun({ data, migration, knex, development })) {
+    i++
+    if (
+      await handleAlreadyRun({
+        data,
+        migration,
+        knex,
+        development,
+        last: i === migrations.length,
+      })
+    ) {
       alreadyRun += 1
       continue
     }
